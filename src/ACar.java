d114a36b242b60d1f1c19217c5da0ebcb5ed0e6d
montif16 <Lukas1532@hotmail.com>
2023-11-11 22:17:24 +0100

    yep

---
+++ src/ACar.java
@@ -0,0 +1,7 @@
+abstract class ACar implements Car {
+    String RegistrationNumber = "null";
+    String Make = "null";
+    String Model = "null";
+    int NumberOfDoors = 0;
+
+}
---
+++ src/AFuelCar.java
@@ -0,0 +1,10 @@
+abstract class AFuelCar {
+    abstract String getFuelType(); // should return “Gasoline” or “Diesel”
+
+    // should return how many kilometres the car can drive on 1 litre of fuel
+    public int kpl;
+    int kmPrLitre(int kpl) {
+        this.kmPrLitre = kmPrLitre;
+    }
+}
+
